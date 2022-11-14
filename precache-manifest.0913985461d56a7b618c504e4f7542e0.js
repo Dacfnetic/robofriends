22d9392d6938db14bdd1a5cd20f755408f3450b1
dacfnetic <dacf9x@gmail.com>
2022-11-14 17:49:35 -0600

    Updates

---
+++ precache-manifest.0913985461d56a7b618c504e4f7542e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "51cbd36f280fce4357c1",
+    "url": "/robofriends/static/css/main.1071637a.chunk.css"
+  },
+  {
+    "revision": "51cbd36f280fce4357c1",
+    "url": "/robofriends/static/js/main.d09cf2f7.chunk.js"
+  },
+  {
+    "revision": "877f8ad29b66b8d6fe13",
+    "url": "/robofriends/static/js/runtime~main.f8b0d496.js"
+  },
+  {
+    "revision": "3d3085565cad97c4f499",
+    "url": "/robofriends/static/css/2.77b190d1.chunk.css"
+  },
+  {
+    "revision": "3d3085565cad97c4f499",
+    "url": "/robofriends/static/js/2.eb3c257b.chunk.js"
+  },
+  {
+    "revision": "eac2663a493c35f8869d",
+    "url": "/robofriends/static/js/3.a2d6af6c.chunk.js"
+  },
+  {
+    "revision": "ac05b3cd047d79ab1e530075c7f5826c",
+    "url": "/robofriends/index.html"
+  }
+];
