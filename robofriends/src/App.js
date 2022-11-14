37505fe3edd4e2938811f644724a0e0bb2318ffd
Diego Calderon <dacf9x@gmail.com>
2022-11-14 15:50:26 -0600

    d

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -7,23 +7,39 @@ class App extends Component {
     constructor(){
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
     }
 
-    onSearchChange(event){
-        console.log(event.target.value);
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value});
     }
 
+componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users').then(response=>{
+        return response.json();
+    }).then(users => {
+        this.setState({robots: users});
+    });
+    
+}
+
     render(){
-        return(
-            <div className='tc'>
-                <h1>Roboamigos perrones</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={this.state.robots}/>
-            </div>    
-        );
+        const filteredRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        });
+        if(robots.length === 0){
+            return <h1>Loading</h1>
+        }else{
+            return(
+                <div className='tc'>
+                    <h1 className='f1'>Roboamigos perrones</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <CardList robots={filteredRobots}/>
+                </div>    
+            );
+        }
     }
 }
 
